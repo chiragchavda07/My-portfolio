45830a4ce1223e458dca40af6ab77545ac5dc4a5
Chirag Chavda <202001164@daiict.ac.in>
2023-05-22 16:58:17 +0530

    Made personal portfolio using bootstrap

---
+++ myjs.js
@@ -0,0 +1,21 @@
+function to_about()
+{
+    window.location.href="about.html";
+}
+function to_skills()
+{
+    window.location.href="skills.html";
+}
+function to_experience()
+{
+    window.location.href="experience.html";
+}
+function to_contact()
+{
+    window.location.href="contact.html";
+}
+function to_projects()
+{
+    window.location.href="projects.html";
+}
+
